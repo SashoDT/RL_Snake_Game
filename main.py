1478f2b4635f445a089bdad194281285f81db0f2
Alexander Tashkov <e12123661@student.tuwien.ac.at>
2023-08-04 15:34:16 +0200

    Created the grid system, player only moves within the cells and added a point for the food, that randomly spawns in a cell

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import pygame
+import random
 
 pygame.init()
 
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((720, 720))
 clock = pygame.time.Clock()
 running = True
 dt = 0
+cellSize = screen.get_width() / 18
+circleRadius = 20
 
 currentDirection = None
-playerPosition = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-foodPosition = pygame.Vector2()
+playerPosition = pygame.Vector2(screen.get_width() / 2 - 20, screen.get_height() / 2 - 20)
+foodPosition = pygame.Vector2(random.randint(1, 18) * 40 - 20, random.randint(1, 18) * 40 - 20)
 
 
 def display_text(text, font_size, x, y):
@@ -21,12 +24,19 @@ def display_text(text, font_size, x, y):
     pygame.display.flip()
 
 
+def draw_grid():
+    for x in range(0, screen.get_width(), 40):
+        pygame.draw.line(screen, "white", (x, 0), (x, screen.get_height()))
+    for y in range(0, screen.get_height(), 40):
+        pygame.draw.line(screen, "white", (0, y), (screen.get_width(), y))
+
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    if playerPosition.x <= 0 or playerPosition.x >= screen.get_width() or \
-       playerPosition.y <= 0 or playerPosition.y >= screen.get_height():
+    if playerPosition.x <= 0 + circleRadius - 1 or playerPosition.x >= screen.get_width() - circleRadius + 1 or \
+       playerPosition.y <= 0 + circleRadius - 1 or playerPosition.y >= screen.get_height() - circleRadius + 1:
         display_text("Game Over", 100, screen.get_width() / 2, screen.get_height() / 2)
         display_text("Press R to restart", 50, screen.get_width() / 2, screen.get_height() / 2 + 100)
         currentDirection = None
@@ -34,16 +44,18 @@ while running:
     if currentDirection is not None:
         screen.fill("black")
 
-    pygame.draw.circle(screen, "green", playerPosition, 10)
+    draw_grid()
+    pygame.draw.circle(screen, "green", playerPosition, circleRadius)
+    pygame.draw.circle(screen, "red", foodPosition, circleRadius)
 
     if currentDirection == "left":
-        playerPosition.x -= 300 * dt
+        playerPosition.x -= 40
     elif currentDirection == "right":
-        playerPosition.x += 300 * dt
+        playerPosition.x += 40
     elif currentDirection == "up":
-        playerPosition.y -= 300 * dt
+        playerPosition.y -= 40
     elif currentDirection == "down":
-        playerPosition.y += 300 * dt
+        playerPosition.y += 40
 
     key = pygame.key.get_pressed()
 
@@ -56,12 +68,14 @@ while running:
     if key[pygame.K_RIGHT]:
         currentDirection = 'right'
     if key[pygame.K_r]:
+        currentDirection = None
         screen.fill("black")
-        playerPosition = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+        playerPosition = pygame.Vector2(screen.get_width() / 2 - 20, screen.get_height() / 2 - 20)
+        foodPosition = pygame.Vector2(random.randint(1, 18) * 40 - 20, random.randint(1, 18) * 40 - 20)
 
     pygame.display.flip()
 
-    dt = clock.tick(60) / 1000
+    dt = clock.tick(15) / 1000
 
 pygame.quit()
 
